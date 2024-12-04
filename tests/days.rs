4546161ed6dc6204651fbffe3b3de081ee8968cc
Lucas Farias <lucasfnf@gmail.com>
2024-12-04 08:32:50 -0300

    Merge tests into single file

---
+++ tests/days.rs
@@ -0,0 +1,33 @@
+#[test]
+fn day1() {
+    let data = r#"3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"#;
+    assert_eq!(aoc2024::day1::part1(data.as_bytes()), 11);
+    assert_eq!(aoc2024::day1::part2(data.as_bytes()), 31);
+}
+
+#[test]
+fn day2() {
+    let data = r#"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"#;
+    assert_eq!(aoc2024::day2::part1(data.as_bytes()), 2);
+    assert_eq!(aoc2024::day2::part2(data.as_bytes()), 4);
+}
+
+#[test]
+fn day3() {
+    let data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    assert_eq!(aoc2024::day3::part1(data.as_bytes()), 161);
+    let data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    assert_eq!(aoc2024::day3::part2(data.as_bytes()), 48);
+}
